5138360afe14f67c6254a0e110aaf9865e2ca32a
Agata Ciuchta <ciuchta@student.chalmers.se>
2021-11-16 11:17:08 +0100

    customer

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,44 @@
+
 public class Customer {
-    // test
-    //new test
-}
+
+        private String name;
+        private int birthYear;
+        String customerID;
+        String userName;
+        String userPassword;
+        double currentBalance;
+
+        Customer(String name, int birthYear, String customerID, String userName, String userPassword){
+            this.name = name;
+            this.birthYear = birthYear;
+            this.customerID = customerID;
+            this.userName = userName;
+            this.userPassword = userPassword;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getBirthYear() {
+            return birthYear;
+        }
+
+        public String getCustomerID() {
+            return customerID;
+        }
+
+        public String getUserName() {
+            return userName;
+        }
+
+        public String getUserPassword() {
+            return userPassword;
+        }
+        public double depositMoney(double depositAmount){
+            currentBalance =- depositAmount;
+            return currentBalance;
+        }
+        public double wi
+    }
+
