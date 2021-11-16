1ee1f36a325f3be8ea733abe7803d92d13de0510
jenniferhalgh <halgh@student.chalmers.se>
2021-11-16 11:58:46 +0100

    readinput

---
+++ src/Controller.java
@@ -0,0 +1,11 @@
+package src;
+import java.util.ArrayList;
+
+public class Controller {
+
+    ArrayList<String> newCardRequests = new ArrayList<>();
+    ArrayList<String> loanRequests = new ArrayList<>();
+    ArrayList<String> mortgageRequests = new ArrayList<>();
+    ArrayList<String> accountRequests = new ArrayList<>();
+
+}
