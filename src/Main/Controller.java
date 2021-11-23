cde8db979ef28d3aaca4d36cbb614188685b5d8e
elias <eliassam@net.chalmers.se>
2021-11-23 11:11:10 +0100

    update

--- src/Main/Controller.java
+++ src/Main/Controller.java
@@ -6,8 +6,42 @@ public class Controller {
 
 
     //Employee
+
+    public void createEmploye(){}
+
+    public void createManager(){}
+
     public void takeDaysof(int amountDays) { }
 
     public void approveMortageRequest(String customerID){}
+
+    public void removeEmployee(String emID){}
+
+    public void promoteEmployee(String emID){}
+
+    public void removeCustomerAccount(String customerID){}
+
+    public void sendMessageToCustomer(String customerID){} //another argument
+
+    public String getCustomerInfo(String customerID){
+        return "";
+    }
+
+    public void updateEmployeName(String emID, String newName){}
+
+    public void updateEmployeSalary(String emID, double newGrossSalary){}
+
+
+    //Customer
+    public void createBankAccount(String name, int birthYear, String customerID, String userName, String userPassword){}
+
+    public double checkBalance(){
+        return 0;
+    }
+
+
+
+
+
 
 }
