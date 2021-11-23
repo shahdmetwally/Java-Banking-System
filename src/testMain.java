1f4ac0f04a862ccbf7c848f8bc2961e7616f0736
pokkias <pokkias@student.chalmers.se>
2021-11-23 11:11:59 +0100

    test

--- src/testMain.java
+++ src/testMain.java
@@ -1,12 +1,12 @@
-import Classes.Customer;
-import Classes.Employee;
+import Classes.*;
+
 
 public class testMain {
     public static void main(String [] args){
         System.out.println("hello");
         Customer customer = new Customer("Marta", 900110, "900104", "900104", "asd");
         System.out.println(customer);
-        //that th
+        //that booboo
 
     }
 }
